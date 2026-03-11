79eb249f1d33062d75231de496fe1470316d0652
MtScience <107070506+MtScience@users.noreply.github.com>
2026-03-11 16:50:03 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+from cmdline_options import extract_cmdline_options, verify_options
+from exam_results import read_exam_data
+from reports import Reporter
+
+
+def main() -> None:
+    reporter = Reporter()
+    options = extract_cmdline_options()
+    verify_options(reporter.report_types, options)
+
+    exam_data = read_exam_data(options.files)
+    reporter.generate_report(options.report, exam_data)
+
+
+if __name__ == '__main__':
+    main()
